7daa30bbd1f8d6851ea112483dc94f5f0d1d31ac
Dmitry Chestnykh <dmitry@codingrobots.com>
2013-11-14 10:33:25 +0100

    Update for Dart 1.0.

--- example/example.dart
+++ example/example.dart
@@ -1,10 +1,10 @@
-import 'dart:utf';
+import 'dart:convert';
 import 'package:crypto/crypto.dart';
 import '../lib/blake2.dart';
 
 main() {
   var h = new BLAKE2s();
-  h.add(encodeUtf8("I want to hash this string"));
+  h.add(UTF8.encode("I want to hash this string"));
   var digest = h.close();
   print(CryptoUtils.bytesToHex(digest));
 }
